ed9161ae3f1a708b827e8151c16d1ce6e48695bf
Barry Kooij <b.kooij@gmail.com>
2014-08-11 21:06:11 +0200

    Rearranged constants.

--- classes/class-constants.php
+++ classes/class-constants.php
@@ -6,14 +6,16 @@ if ( !defined( 'ABSPATH' ) ) {
 
 abstract class RP4WP_Constants {
 
-//	const LINK_TITLE = 'rp4wp_link';
+	// Link title
 	const LINK_PT = 'rp4wp_link';
 
-	// Post meta
+	// Linked meta
 	const PM_PARENT = 'rp4wp_parent';
 	const PM_CHILD = 'rp4wp_child';
-	const PM_CACHED = 'rp4wp_cached';
-	const PM_AUTO_LINKED = 'rp4wp_auto_linked';
+
+	// Post meta
+	const PM_CACHED = 'rp4wp_cached'; // Posts that words are saved of
+	const PM_POST_AUTO_LINKED = 'rp4wp_auto_linked'; // Posts that have automatically linked posts
 
 	// Options
 	const OPTION_DO_INSTALL = 'rp4wp_do_install';
