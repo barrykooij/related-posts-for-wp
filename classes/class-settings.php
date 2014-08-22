8a1dcd683c71018d755800e2c8dd65df0e1df15b
Barry Kooij <b.kooij@gmail.com>
2014-08-22 20:15:40 +0200

    Small default CSS tweaks. props @savvyjackie

--- classes/class-settings.php
+++ classes/class-settings.php
@@ -25,10 +25,10 @@ class RP4WP_Settings {
 
 		// CSS default
 		$css_default_lines = array();
-		$css_default_lines[] = '.rp4wp-related-posts ul {width:100%padding:0;margin:0;float:left;}';
-		$css_default_lines[] = '.rp4wp-related-posts li{list-style:none;padding:0;margin:0;padding-bottom:20px;}';
-		$css_default_lines[] = '.rp4wp-related-posts li p {margin:0;padding:0}';
-		$css_default_lines[] = '.rp4wp-related-post-image{width:35%;padding-right:25px;-moz-box-sizing: border-box;-webkit-box-sizing: border-box;box-sizing: border-box;float:left;}';
+		$css_default_lines[] = '.rp4wp-related-posts ul{width:100%;padding:0;margin:0;float:left;}';
+		$css_default_lines[] = '.rp4wp-related-posts ul>li{list-style:none;padding:0;margin:0;padding-bottom:20px;clear:both;}';
+		$css_default_lines[] = '.rp4wp-related-posts ul>li>p{margin:0;padding:0}';
+		$css_default_lines[] = '.rp4wp-related-post-image{width:35%;padding-right:25px;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;box-sizing:border-box;float:left;}';
 
 		// The fields
 		$this->sections = array(
