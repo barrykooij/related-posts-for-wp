dfcbca79b4cf76595dfedc793cddb8978e8ab504
Barry Kooij <b.kooij@gmail.com>
2014-08-24 20:23:11 +0200

    Added 'settings' link to plugins links. Fixes #31

---
+++ classes/filters/class-filter-plugin-links.php
@@ -0,0 +1,25 @@
+<?php
+
+if ( !defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+class RP4WP_Filter_Plugin_Links extends RP4WP_Filter {
+	protected $tag = 'plugin_action_links_related-posts-for-wp/related-posts-for-wp.php';
+
+	/**
+	 * Add custom plugin links
+	 *
+	 * @param array $links
+	 *
+	 * @since  1.4.0
+	 * @access public
+	 *
+	 * @return array
+	 */
+	public function run( $links ) {
+		array_unshift( $links, '<a href="' . admin_url( 'options-general.php?page=rp4wp' ) . '">' . __( 'Settings', 'related-posts-for-wp' ) . '</a>' );
+
+		return $links;
+	}
+}
