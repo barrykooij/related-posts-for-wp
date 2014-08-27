d1316672c0ea30a03795c88b64f8d57ecef35e61
Barry Kooij <b.kooij@gmail.com>
2014-08-27 22:32:42 +0200

    Add class_exists wrapper

--- classes/class-manager-hook.php
+++ classes/class-manager-hook.php
@@ -4,53 +4,57 @@ if ( !defined( 'ABSPATH' ) ) {
 	exit;
 } // Exit if accessed directly
 
-class RP4WP_Manager_Hook {
+if ( !class_exists( 'RP4WP_Manager_Hook' ) ) {
 
-	private $hook_dir;
-	private static $hooks;
+	class RP4WP_Manager_Hook {
 
-	public function __construct( $hook_dir ) {
-		$this->hook_dir = $hook_dir;
-	}
+		private $hook_dir;
+		private static $hooks;
 
-	/**
-	 * Load and set hooks
-	 *
-	 * @access public
-	 * @static
-	 * @return void
-	 */
-	public function load_hooks() {
+		public function __construct( $hook_dir ) {
+			$this->hook_dir = $hook_dir;
+		}
 
-		foreach ( new DirectoryIterator( $this->hook_dir ) as $file ) {
-			$file_name = $file->getFileName();
+		/**
+		 * Load and set hooks
+		 *
+		 * @access public
+		 * @static
+		 * @return void
+		 */
+		public function load_hooks() {
 
-			if ( !$file->isDir() && ( strpos( $file->getFileName(), '.' ) !== 0 ) ) {
+			foreach ( new DirectoryIterator( $this->hook_dir ) as $file ) {
+				$file_name = $file->getFileName();
 
-				$class = RP4WP_Class_Manager::format_class_name( $file->getFileName() );
-				if ( 'RP4WP_Hook' != $class ) {
-					self::$hooks[$class] = new $class;
+				if ( !$file->isDir() && ( strpos( $file->getFileName(), '.' ) !== 0 ) ) {
+
+					$class = RP4WP_Class_Manager::format_class_name( $file->getFileName() );
+					if ( 'RP4WP_Hook' != $class ) {
+						self::$hooks[$class] = new $class;
+					}
+
 				}
 
 			}
 
 		}
 
-	}
+		/**
+		 * Return instance of created hook
+		 *
+		 * @param $class_name
+		 *
+		 * @return Hook
+		 */
+		public static function get_hook_instance( $class_name ) {
+			if ( isset( self::$hooks[$class_name] ) ) {
+				return self::$hooks[$class_name];
+			}
 
-	/**
-	 * Return instance of created hook
-	 *
-	 * @param $class_name
-	 *
-	 * @return Hook
-	 */
-	public static function get_hook_instance( $class_name ) {
-		if ( isset( self::$hooks[$class_name] ) ) {
-			return self::$hooks[$class_name];
+			return null;
 		}
 
-		return null;
 	}
 
 }
