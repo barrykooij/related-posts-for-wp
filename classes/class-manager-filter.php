43daac06f982668452a823db1f3e9343f06ea722
Barry Kooij <b.kooij@gmail.com>
2014-08-27 22:33:53 +0200

    Added class_exists to hook manager

--- classes/class-manager-filter.php
+++ classes/class-manager-filter.php
@@ -1,68 +1,73 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
+if ( !defined( 'ABSPATH' ) ) {
 	exit;
 } // Exit if accessed directly
 
-class RP4WP_Manager_Filter {
+if ( !class_exists( 'RP4WP_Manager_Filter' ) ) {
 
-	private $filter_dir;
-	private static $filters;
+	class RP4WP_Manager_Filter {
 
-	public function __construct( $filter_dir ) {
-		$this->filter_dir = $filter_dir;
-	}
+		private $filter_dir;
+		private static $filters;
 
-	/**
-	 * Load on specific filter instead of all filters.
-	 * This method should be used when the load_filters() isn't run yet, for example in the (de)activation process.
-	 *
-	 * @param $file_name
-	 */
-	public function load_filter( $file_name ) {
-		$class = RP4WP_Class_Manager::format_class_name( $file_name );
-		if ( 'RP4WP_Filter' != $class ) {
-			self::$filters[$class] = new $class;
+		public function __construct( $filter_dir ) {
+			$this->filter_dir = $filter_dir;
 		}
-	}
 
-	/**
-	 * Load and set hooks
-	 *
-	 * @access public
-	 * @static
-	 * @return void
-	 */
-	public function load_filters() {
-
-		foreach ( new DirectoryIterator( $this->filter_dir ) as $file ) {
-			$file_name = $file->getFileName();
-
-			if ( ! $file->isDir() && ( strpos( $file->getFileName(), '.' ) !== 0 ) ) {
-
-				$class = RP4WP_Class_Manager::format_class_name( $file->getFileName() );
-				if ( 'RP4WP_Filter' != $class ) {
-					self::$filters[$class] = new $class;
-				}
-
+		/**
+		 * Load on specific filter instead of all filters.
+		 * This method should be used when the load_filters() isn't run yet, for example in the (de)activation process.
+		 *
+		 * @param $file_name
+		 */
+		public function load_filter( $file_name ) {
+			$class = RP4WP_Class_Manager::format_class_name( $file_name );
+			if ( 'RP4WP_Filter' != $class ) {
+				self::$filters[$class] = new $class;
 			}
 		}
 
-	}
+		/**
+		 * Load and set hooks
+		 *
+		 * @access public
+		 * @static
+		 * @return void
+		 */
+		public function load_filters() {
 
-	/**
-	 * Return instance of created hook
-	 *
-	 * @param $class_name
-	 *
-	 * @return Hook
-	 */
-	public static function get_filter_instance( $class_name ) {
-		if ( isset( self::$filters[$class_name] ) ) {
-			return self::$filters[$class_name];
+			foreach ( new DirectoryIterator( $this->filter_dir ) as $file ) {
+				$file_name = $file->getFileName();
+
+				if ( !$file->isDir() && ( strpos( $file->getFileName(), '.' ) !== 0 ) ) {
+
+					$class = RP4WP_Class_Manager::format_class_name( $file->getFileName() );
+					if ( 'RP4WP_Filter' != $class ) {
+						self::$filters[$class] = new $class;
+					}
+
+				}
+			}
+
 		}
 
-		return null;
-	}
+		/**
+		 * Return instance of created hook
+		 *
+		 * @param $class_name
+		 *
+		 * @return Hook
+		 */
+		public static function get_filter_instance( $class_name ) {
+			if ( isset( self::$filters[$class_name] ) ) {
+				return self::$filters[$class_name];
+			}
 
+			return null;
+		}
+
+	}
+	
 }
+
