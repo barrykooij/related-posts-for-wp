4129c06741f77bbaee50e1c830dcd398b4ee8ac7
Barry Kooij <b.kooij@gmail.com>
2014-09-05 20:41:13 +0200

    use passed ppq

--- classes/hooks/class-hook-ajax-install-save-words.php
+++ classes/hooks/class-hook-ajax-install-save-words.php
@@ -9,11 +9,13 @@ class RP4WP_Hook_Ajax_Install_Save_Words extends RP4WP_Hook {
 
 	public function run() {
 
+		$rel_amount = isset( $_POST['rel_amount'] ) ? $_POST['rel_amount'] : 100;
+
 		// Related Post Manager
 		$related_word_manager = new RP4WP_Related_Word_Manager();
 
 		// Save 200 words
-		if ( true === $related_word_manager->save_all_words( 200 ) ) {
+		if ( true === $related_word_manager->save_all_words( $rel_amount ) ) {
 
 			// Check if we're done
 			if ( 0 == count( $related_word_manager->get_uncached_posts( 1 ) ) ) {
