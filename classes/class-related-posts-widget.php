e684d361de4c7fc98723c0445200616f9ccbf888
Barry Kooij <b.kooij@gmail.com>
2014-09-05 21:45:35 +0200

    Widget tweaks

--- classes/class-related-posts-widget.php
+++ classes/class-related-posts-widget.php
@@ -20,12 +20,24 @@ class RP4WP_Related_Posts_Widget extends WP_Widget {
 		// Get the current ID
 		$id = get_the_ID();
 
+		// Not on frontpage please
+		if ( is_front_page() && false === is_page() ) {
+			return;
+		}
+
 		// Post Link Manager
 		$pl_manager = new RP4WP_Post_Link_Manager();
 
-		// Output the widget
-		echo $args['before_widget'];
-		echo $pl_manager->generate_children_list( $id );
-		echo $args['after_widget'];
+		// Get content
+		$widget_content = $pl_manager->generate_children_list( $id );
+
+		// Only display if there's content
+		if(''!=$widget_content) {
+			// Output the widget
+			echo $args['before_widget'];
+			echo $widget_content;
+			echo $args['after_widget'];
+		}
+
 	}
 }
