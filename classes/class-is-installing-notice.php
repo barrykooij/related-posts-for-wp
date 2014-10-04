abd94db02548d7896c0ade4bf2026263b4a5ab69
Barry Kooij <b.kooij@gmail.com>
2014-10-04 21:53:24 +0200

    Made check more strict.

--- classes/class-is-installing-notice.php
+++ classes/class-is-installing-notice.php
@@ -42,7 +42,7 @@ if ( ! class_exists( 'RP4WP_Is_Installing_Notice' ) ) {
 			}
 
 			// Check if we are currently installing
-			if ( get_site_option( RP4WP_Constants::OPTION_IS_INSTALLING, false ) && ( ! isset( $_GET['page'] ) || 'rp4wp_install' != $_GET['page'] ) ) {
+			if ( false != get_site_option( RP4WP_Constants::OPTION_IS_INSTALLING, false ) && ( ! isset( $_GET['page'] ) || 'rp4wp_install' != $_GET['page'] ) ) {
 				$this->display();
 			}
 
