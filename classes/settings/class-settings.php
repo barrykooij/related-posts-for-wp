017f248a92b3fcd4f533fb741e77fa162d367314
Barry Kooij <b.kooij@gmail.com>
2014-10-04 22:42:58 +0200

    Moved settings class to own directory.

--- classes/settings/class-settings.php
+++ classes/settings/class-settings.php
@@ -96,7 +96,7 @@ class RP4WP_Settings {
 						'label'       => __( 'Restart the wizard?', 'related-posts-for-wp' ),
 						'description' => __( "Click this button if you want to restart the wizard. Please note that this will delete all current related post links, also those you've manually added. Of course, we will never delete your actual posts.", 'related-posts-for-wp' ),
 						'type'        => 'button_link',
-						'href'        => admin_url( '?page=rp4wp_install&reinstall=1&rp4wp_nonce=' . wp_create_nonce( RP4WP_Constants::NONCE_REINSTALL ) ),
+						'href'        => admin_url( '?page=rp4wp_install&reinstall=1&rp4wp_nonce=' . wp_create_nonce( RP4WP_Constants::NONCE_INSTALL ) ),
 						'default'     => __( 'Restart wizard', 'related-posts-for-wp' ),
 					),
 				) ),
