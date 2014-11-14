4ad721aa222ad0eaeff7d1df5036f37a210a7e33
Barry Kooij <git@barrykooij.nl>
2014-11-15 00:54:48 +0100

    URL update

--- classes/class-multisite-notice.php
+++ classes/class-multisite-notice.php
@@ -9,7 +9,7 @@ class RP4WP_Multisite_Notice {
 		echo '<div class="error"><p>';
 		printf( __( "The free version of Related Posts for WordPress doesn't support WordPress Multisite/Network!" ), '<b>', '</b>' );
 		echo "<br /><br />";
-		printf( __( "%sBuy the premium version%s or disable the plugin." ), '<a href="https://www.relatedpostsforwp.com/?utm_source=plugin&utm_medium=link&utm_campaign=multisite-notice" target="_blank">', '</a>' );
+		printf( __( "%sBuy the premium version%s or disable the plugin." ), '<a href="https://www.relatedpostsforwp.com/upgrade-premium/?utm_source=plugin&utm_medium=link&utm_campaign=multisite-notice" target="_blank">', '</a>' );
 		echo "</p></div>";
 	}
 }
