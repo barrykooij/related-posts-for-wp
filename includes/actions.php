e498d0b730621616da568354a594b7ec1569b7dc
Danny van Kooten <dannyvankooten@gmail.com>
2015-05-03 16:35:27 +0200

    static hook loading (instead of directory scan)

---
+++ includes/actions.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+	'admin_scripts',
+	'ajax_delete_link',
+	'ajax_install_link_posts',
+	'ajax_install_save_words',
+	'delete_words',
+	'frontend_css',
+	'link_related_screen',
+	'meta_box',
+	'meta_box_ajax_sort',
+	'page_install',
+	'post_type',
+	'related_auto_link',
+	'related_save_words',
+	'settings_page',
+	'shortcode',
+	'widget'
+);
