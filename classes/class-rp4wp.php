ed7b0fe252f5703c0800b1798e7899d70f934bab
Barry Kooij <git@barrykooij.nl>
2017-09-07 12:03:03 +0200

    Introduced Dependencies class. Now checing for mbstring PHP ext. Fixes #97

--- classes/class-rp4wp.php
+++ classes/class-rp4wp.php
@@ -79,6 +79,10 @@ class RP4WP {
 			// Check if we need to display an 'is installing' notice
 			$is_installing_notice = new RP4WP_Is_Installing_Notice();
 			$is_installing_notice->check();
+
+			// check for dependencies
+			$dep = new RP4WP_Dependencies();
+			$dep->check();
 		}
 
 		// Setup settings
